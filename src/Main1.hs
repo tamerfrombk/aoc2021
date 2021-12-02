489e979e1b2aed08d43a1c4ea113d3b223a66cd8
Tamer Aly <taly.dev75@gmail.com>
2021-12-02 18:46:03 -0500

    day 1, puzzle 1

--- src/Main1.hs
+++ src/Main1.hs
@@ -1,4 +1,32 @@
 module Main where
 
+import System.Environment ( getArgs )
+
+type Depth = Int
+
+makeDepths :: String -> [Depth]
+makeDepths = map (\c -> read c :: Depth) . lines
+
+increases :: [Depth] -> Int
+increases (x:y:xs)
+    | x < y     = 1 + increases (y:xs)
+    | otherwise = increases (y:xs)
+increases _     = 0
+
+solve1 :: String -> String
+solve1 = show . increases . makeDepths
+
+solve2 :: String -> String
+solve2 = undefined
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs (n:path:_)
+    | n == "1"  = invoke solve1
+    | n == "2"  = invoke solve2
+    | otherwise = error $ "invalid puzzle number" <> n
+    where invoke :: (String -> String) -> IO ()
+          invoke f = readFile path >>= putStrLn . f
+mainWithArgs _  = error "invalid arguments: <puzzle> <input_file> required"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = getArgs >>= mainWithArgs
