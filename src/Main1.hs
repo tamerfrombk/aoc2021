3668b0acc926ee711fb474d62bd7d2a745036500
Tamer Aly <taly.dev75@gmail.com>
2021-12-02 19:10:53 -0500

    remove unnecessary show call

--- src/Main1.hs
+++ src/Main1.hs
@@ -13,15 +13,15 @@ increases (x:y:xs)
     | otherwise = increases (y:xs)
 increases _     = 0
 
-solve1 :: String -> String
-solve1 = show . increases . makeDepths
+solve1 :: String -> Int
+solve1 = increases . makeDepths
 
 slideSums :: [Depth] -> [Depth]
 slideSums (a:b:c:rest) = sum [a,b,c] : slideSums (b:c:rest)
 slideSums _            = []
 
-solve2 :: String -> String
-solve2 = show . increases . slideSums . makeDepths
+solve2 :: String -> Int
+solve2 = increases . slideSums . makeDepths
 
 main :: IO ()
 main = mainImpl solve1 solve2
