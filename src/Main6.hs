ff020333e7b484451495134f98ff32bcbd50ec21
Tamer Aly <taly.dev75@gmail.com>
2021-12-10 11:20:51 -0500

    6,1

--- src/Main6.hs
+++ src/Main6.hs
@@ -1,9 +1,27 @@
 module Main where
 
-import Util ( mainImpl )
+import Util ( mainImpl, parseNumsBy )
 
-solve1 :: String -> String
-solve1 = id
+newtype Fish = Fish Int
+    deriving (Show, Eq)
+
+tick :: Fish -> [Fish]
+tick (Fish n)
+    | n == 0    = [Fish 6, Fish 8]
+    | otherwise = [Fish (n - 1)]
+
+lifetime :: Int -> [Fish] -> [Fish]
+lifetime n = go 0
+    where go :: Int -> [Fish] -> [Fish]
+          go i fs
+            | i == n    = fs
+            | otherwise = concatMap tick $ go (i + 1) fs
+
+parseFish :: String -> [Fish]
+parseFish = map Fish . parseNumsBy ','
+
+solve1 :: String -> Int
+solve1 = length . lifetime 80 . parseFish
 
 solve2 :: String -> String
 solve2 = id
