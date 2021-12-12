aaf4b02cd4aace4ffa18dbdb49bd80a62b2b7cc4
Tamer Aly <taly.dev75@gmail.com>
2021-12-12 00:20:55 -0500

    7,1

--- src/Main7.hs
+++ src/Main7.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import Util ( mainImpl )
+import Util ( mainImpl, parseNumsBy )
+
+fuelTo :: Int -> [Int] -> Int
+fuelTo p = sum . map (\n ->abs $ n - p)
 
 solve1 :: String -> Int
-solve1 = undefined
+solve1 s = let nums = parseNumsBy ',' s in minimum $ map (`fuelTo` nums) [1 .. length nums]
 
 solve2 :: String -> Int
 solve2 = undefined
