cfef56774a290e7ae3c2616cfc8f86a426f2b6d7
Tamer Aly <taly.dev75@gmail.com>
2021-12-12 02:32:30 -0500

    day 8 init

---
+++ src/Main8.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Util ( mainImpl )
+
+solve1 :: String -> Int
+solve1 = undefined
+
+solve2 :: String -> Int
+solve2 = undefined
+
+main :: IO ()
+main = mainImpl solve1 solve2
