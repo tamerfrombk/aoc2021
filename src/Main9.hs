aa48462ec7222229f0526d0906a1f842ac9fede7
Tamer Aly <taly.dev75@gmail.com>
2021-12-16 22:19:09 -0500

    slightly improve the runtime complexity of 9,1 from 3 * O (n) to O(n)

--- src/Main9.hs
+++ src/Main9.hs
@@ -19,19 +19,20 @@ adjacents r c hm = mapMaybe (\(r', c') -> at r' c' hm) [
     , (r, c)     -- current
     ]
 
-findLowestPoints :: HeightMap -> [Int]
-findLowestPoints hm = filter (/=9) -- smoke can't settle on the highest point
-    $ foldr appendIfMinimum [] indexes
+calculateRiskOfLowestPoints :: HeightMap -> [Int]
+calculateRiskOfLowestPoints hm = foldr appendIfMinimum [] indexes
     where appendIfMinimum :: (Int, Int) -> [Int] -> [Int]
           appendIfMinimum (r, c) acc = let curr = fromJust $ at r c hm
-              in if curr == minimum (adjacents r c hm) then curr : acc else acc
+              in case curr of
+                  9 -> acc -- smoke can't settle on the highest point (9) so we exclude it
+                  _ -> if curr == minimum (adjacents r c hm) then curr + 1 : acc else acc
           indexes = [(r, c) | r <- [0 .. length hm - 1], c <- [0 .. length (head hm) - 1]]
 
 parseHeightMap :: String -> HeightMap
 parseHeightMap = map (map digitToInt) . lines
 
 solve1 :: String -> Int
-solve1 = sum . map (+1) . findLowestPoints . parseHeightMap
+solve1 = sum . calculateRiskOfLowestPoints . parseHeightMap
 
 solve2 :: String -> Int
 solve2 = undefined
